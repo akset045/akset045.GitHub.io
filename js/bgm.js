4cdab38d3f27c502d7f2294760f3af11c0922ee9
akset045 <akset045@gmail.com>
2022-05-22 19:57:28 +0300

    Well its working

--- js/bgm.js
+++ js/bgm.js
@@ -1,12 +1,56 @@
-function playpause() {
-	var snd = document.getElementById("sound");
-	var snd_btn = document.getElementById("SoundButton");
-	snd.muted = !snd.muted;
+let SoundButton = document.getElementById("SoundButton");
+let ThemeButton = document.getElementById("ThemeButton");
 
-	if(snd.muted) {
-		snd_btn.innerHTML = "<img src='src/switch-ani-on-off.gif' alt='Here's an audio control' id='SoundIcon'>";
+let BGM = document.getElementById("SoundEl");
+let BGMIcon = document.getElementById("SoundIcon");
+
+let theme = document.getElementById('ThemeNow');
+let themeIcon = document.getElementById('ThemeIcon');
+
+SoundButton.onclick = function () {
+
+	BGM.muted = !BGM.muted;
+
+	if (BGM.muted) {
+		if (theme.getAttribute('href') == 'css/1-styles-light.css') {
+			BGMIcon.src = 'src/sound-off-dark.png'
+		}
+		else {
+			BGMIcon.src = 'src/sound-off-white.png'
+		}
 	}
 	else {
-		snd_btn.innerHTML = "<img src='src/sound-on.gif' alt='Here's an audio control' id='SoundIcon'>";
+		if (theme.getAttribute('href') == 'css/1-styles-light.css') {
+			BGMIcon.src = 'src/sound-on-dark.png'
+		}
+		else {
+			BGMIcon.src = 'src/sound-on-white.png'
+		}
+	}
+}
+
+ThemeButton.onclick = function () {
+
+	if (theme.getAttribute('href') == 'css/1-styles-light.css') {
+		theme.href = 'css/2-styles-dark.css'
+		themeIcon.src = 'src/theme-moon.png'
+
+		if (BGM.muted) {
+			BGMIcon.src = 'src/sound-off-white.png'
+		}
+		else {
+			BGMIcon.src = 'src/sound-on-white.png'
+		}
+	}
+	else {
+		theme.href = 'css/1-styles-light.css'
+		themeIcon.src = 'src/theme-sun.gif'
+
+		if (BGM.muted) {
+			BGMIcon.src = 'src/sound-off-dark.png'
+		}
+		else {
+			BGMIcon.src = 'src/sound-on-dark.png'
+		}
 	}
 }
